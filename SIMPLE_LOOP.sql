8dda29d5413861ff9fd6eb405e86aa8b9a37260b
Zibby92 <119049399+Zibby92@users.noreply.github.com>
2023-07-05 11:05:38 +0200

    Add first loop - SIMPLE LOOP

---
+++ SIMPLE_LOOP.sql
@@ -0,0 +1,46 @@
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    v_lp NUMBER := 0;
+BEGIN
+    LOOP
+        v_lp := v_lp+1;
+        DBMS_OUTPUT.PUT_LINE(v_lp);
+        EXIT WHEN v_lp=6;
+    END LOOP;
+END;
+/
+
+-- EXIT koñczy pêtlê
+DECLARE
+    v_lp NUMBER := 0;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('przed petl¹');
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('œrodek pêtli przed EXIT');
+        EXIT;
+        DBMS_OUTPUT.PUT_LINE('œrodek pêtli po EXIT');
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('po pêtli');
+END;
+/
+
+-- dziala tylko w pêtli
+BEGIN
+    EXIT;
+END;
+/
+
+-- continue
+DECLARE
+    v_lp NUMBER := 0;
+BEGIN
+    LOOP
+        v_lp := v_lp+1;
+        EXIT WHEN v_lp=10;
+        CONTINUE WHEN mod(v_lp,2) = 0;
+        DBMS_OUTPUT.PUT_LINE(v_lp);
+    END LOOP;
+END;
+/
